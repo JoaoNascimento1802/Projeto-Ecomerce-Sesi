b427142e9bfaf6ed6c8dcc0605eb97022519b283
João Emanuel <joao.emanuel.pn@gmail.com>
2025-05-13 10:08:03 -0300

    Update PedidoController.java

--- Downloads/ecommerce-sesi-projeto-main/ecommerce-sesi-projeto-main/src/main/java/com/revisao/ecommerce/controllers/PedidoController.java
+++ Downloads/ecommerce-sesi-projeto-main/ecommerce-sesi-projeto-main/src/main/java/com/revisao/ecommerce/controllers/PedidoController.java
@@ -1,34 +1,33 @@
 package com.revisao.ecommerce.controllers;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.revisao.ecommerce.dto.PedidoDTO;
 import com.revisao.ecommerce.services.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import jakarta.validation.Valid;
+import java.net.URI;
+import java.util.List;
 
 @RestController
-@RequestMapping("/order")
-public class PedidoController
-{
+@RequestMapping(value = "/pedidos")
+public class PedidoController {
 
     @Autowired
-    PedidoService pedidoService;
+    private PedidoService pedidoService;
 
-    @PostMapping("/new")
-    public ResponseEntity<PedidoDTO> inserPedido(@RequestBody @Valid PedidoDTO dto) {
+    @GetMapping ( value =  "/all")
+    public ResponseEntity<List<PedidoDTO>> getAllPedidos(){
+        return ResponseEntity.ok(pedidoService.findAll());
+    }
+
+    @PostMapping
+    public ResponseEntity<PedidoDTO> insert(@RequestBody PedidoDTO dto) {
         dto = pedidoService.inserir(dto);
-        return ResponseEntity.ok(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(dto.getId()).toUri();
+        return ResponseEntity.created(uri).body(dto);
     }
 }
